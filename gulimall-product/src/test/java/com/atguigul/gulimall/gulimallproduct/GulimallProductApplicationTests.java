d3d876a446eda97533655866ec63690234d5fa70
“zhangqi <2994182080qq.com>
2020-06-24 15:24:31 +0800

    项目初始结构创建

---
+++ gulimall-product/src/test/java/com/atguigul/gulimall/gulimallproduct/GulimallProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigul.gulimall.gulimallproduct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
